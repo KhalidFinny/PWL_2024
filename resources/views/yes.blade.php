9f57909ec5489d1b925f89a0d7e284f7c76be624
finny <katthoriq@gmail.com>
2025-02-17 09:58:45 +0700

    Praktikum 1 Routes

---
+++ resources/views/yes.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hehe</title>
+    </head>
+    <body>
+        <h1 class="font-medium text-center text-3xl text-red-500 hover:text-red-600 mt-20">
+            Hehe
+        </h1>
+        <h3 class="font-medium text-2xl text-center text-red-500 hover:text-red-600">
+            YES
+        </h3>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,43 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/hello', function () {
+    return 'Hello World';
+    });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/world', function () {
+    return 'World';
+    });
+
+Route::get('/selamat', function () {
+return 'Selamat Datang';
 });
+
+Route::get('/about', function () {
+    return 'Nama: Muhammad khalid Atthoriq <br>
+    NIM: 2341720114';
+    });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function() {
+    $postId = request('id');
+    return 'halaman ke-'.$postId;
+});
+
+Route::get('/user/{name?}', function($name =null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+
+Route::View('/welcome', 'yes'); 
